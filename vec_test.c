4e116aef8c2ed82e93fdb2a30c93c15ecfb15fdd
pithuene <pitsimonhuene@t-online.de>
2021-04-28 22:28:44 +0200

    Add comments to vec_test

--- vec_test.c
+++ vec_test.c
@@ -3,33 +3,43 @@
 #include <stdio.h>
 
 int main(void) {
+  /* Initialize */
   int * my_vec = vec(int, 5);
   assert(veclen(my_vec) == 0);
 
-  int i;
-  for (i = 0; i < 100; i++) {
-    vecpush(my_vec, i);
+  /* Add elements */ {
+    int i;
+    for (i = 0; i < 100; i++) {
+      vecpush(my_vec, i);
+    }
+    assert(veclen(my_vec) == 100);
   }
-  assert(veclen(my_vec) == 100);
 
+  /* Set and get values */
   assert(my_vec[1] == 1);
   my_vec[5] = 7;
   assert(my_vec[5] == 7);
+
+  /* Pop element */
   assert(vecpop(my_vec) == 99);
   assert(veclen(my_vec) == 99);
 
-  int rmu = vecrmu(my_vec, 42);
-  assert(rmu == 42);
-  assert(veclen(my_vec) == 98);
-  assert(my_vec[42] == 98);
+  /* Unordered Remove */ {
+    int rmu = vecrmu(my_vec, 42);
+    assert(rmu == 42);
+    assert(veclen(my_vec) == 98);
+    assert(my_vec[42] == 98);
+  }
 
-  int rm = vecrm(my_vec, 42);
-  assert(rm == 98);
-  assert(veclen(my_vec) == 97);
-  assert(my_vec[42] == 43);
+  /* Ordered Remove */ {
+    int rm = vecrm(my_vec, 42);
+    assert(rm == 98);
+    assert(veclen(my_vec) == 97);
+    assert(my_vec[42] == 43);
+  }
 
+  /* Cleanup */
   vecfree(my_vec);
-
   return 0;
 }
 
