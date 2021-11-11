9b7a700d82602b241dc7fcbbae51bbe7324badb7
pithuene <pitsimonhuene@t-online.de>
2021-11-11 11:23:03 +0100

    Increase item count in map_test

--- ds/map_test.c
+++ ds/map_test.c
@@ -1,5 +1,7 @@
 #include "map.h"
+#include "../mem/arenaalloc.h"
 #include <assert.h>
+#include <stdio.h>
 
 typedef struct {
   int number; 
@@ -9,28 +11,31 @@ typedef struct {
 int main(void) {
   map_t(Struct) my_map = map(Struct, 4);
 
+  ArenaAllocator arena = newArenaAllocator(1024);
+
   /* Put */ {
-    Struct s1;
-    s1.number = 123;
-    s1.string = "Some String";
-    Struct s2;
-    s2.number = 456;
-    s2.string = "Some other String";
-    mapput(my_map, "key1", s1);
-    mapput(my_map, "key2", s2);
-    mapput(my_map, "key3", s1);
-    mapput(my_map, "key4", s1);
-    mapput(my_map, "key5", s1);
+    int i;
+    char * str;
+    Struct s;
+    for (i = 0; i < 1024; i++) {
+      str = arenaalloc(&arena, 8);
+      sprintf(str, "key%d", i);
+      Struct s;
+      s.number = i;
+      s.string = "Test123";
+      mapput(my_map, str, s);
+    }
   }
 
   /* Get */ {
-    Struct s1 = mapget(my_map, "key5");
-    assert(s1.number == 123);
-    Struct s2 = mapget(my_map, "key2");
-    assert(s2.number == 456);
+    Struct s123 = mapget(my_map, "key123");
+    assert(s123.number == 123);
+    Struct s456 = mapget(my_map, "key456");
+    assert(s456.number == 456);
   }
 
   mapfree(my_map);
+  deleteArenaAllocator(&arena);
   return 0;
 }
 
