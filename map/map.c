1320bcec911f26942146a60a0821ce207baf9fd8
Pit Hüne <pitsimonhuene@t-online.de>
2022-09-07 12:06:50 +0200

    map, set: Add missing string.h imports

--- map/map.c
+++ map/map.c
@@ -1,6 +1,7 @@
 #include "map.h"
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../util/container_of.h"
 
--- set/set.c
+++ set/set.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "../util/container_of.h"
 
