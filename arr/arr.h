23303e456e5d2aad8f09ff9876515f84bb2a30b7
Pit Hüne <pitsimonhuene@t-online.de>
2022-09-08 18:41:36 +0200

    arr: Use container_of to implement arr_t

--- arr/arr.h
+++ arr/arr.h
@@ -2,8 +2,10 @@
 #define DS_ARR_H
 
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include "../util/container_of.h"
 
 #ifndef DS_NO_SHORT_NAMES
 #define arr_t       ds_arr_t
@@ -15,12 +17,22 @@
 #define arr_bsearch ds_arr_bsearch
 #endif /* DS_NO_SHORT_NAMES */
 
+struct __ds_arr_struct_t {
+  uint64_t len;
+  uint64_t values[];
+};
+
 #define ds_arr_t(TYPE) TYPE *
 
 #define ds_arr_create(TYPE, LENGTH, ...) \
-  ((TYPE *) (((char *) &(struct {size_t length; TYPE values[LENGTH]; }){LENGTH, __VA_ARGS__}) + sizeof(size_t)))
+  ((TYPE *) &(struct {                   \
+     uint64_t len;                       \
+     TYPE values[LENGTH];                \
+   }){LENGTH, __VA_ARGS__}               \
+     .values)
 
-#define ds_arr_len(ARR) (*((size_t *) (((char *) ARR) - sizeof(size_t))))
+#define ds_arr_len(ARR) \
+  (ds_container_of(ARR, struct __ds_arr_struct_t, values)->len)
 
 #define ds_arr_get(ARR, IDX) \
   (assert(IDX >= 0), assert(IDX < ds_arr_len(ARR)), ARR[IDX])
